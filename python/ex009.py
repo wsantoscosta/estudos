fc29b671c351c1942cc5a0764697bb08ef164341
Washington S Costa <wasanco@outlook.com>
2022-10-18 16:09:04 -0300

    Review

--- python/ex009.py
+++ python/ex009.py
@@ -3,9 +3,19 @@
 numero = int(input("Entre com um numéro inteiro: "))
 print("===========")
 
-for i in range(1,11):
-    print("{} x {:>2} = {:>2}".format(numero, i, numero * i))
+#for i in range(1,11):
+#    print("{} x {:>2} = {:>2}".format(numero, i, numero * i))
 
+print("{} x {:>2} = {:>2}".format(numero, 1, numero * 1))
+print("{} x {:>2} = {:>2}".format(numero, 2, numero * 2))
+print("{} x {:>2} = {:>2}".format(numero, 3, numero * 3))
+print("{} x {:>2} = {:>2}".format(numero, 4, numero * 4))
+print("{} x {:>2} = {:>2}".format(numero, 5, numero * 5))
+print("{} x {:>2} = {:>2}".format(numero, 6, numero * 6))
+print("{} x {:>2} = {:>2}".format(numero, 7, numero * 7))
+print("{} x {:>2} = {:>2}".format(numero, 8, numero * 8))
+print("{} x {:>2} = {:>2}".format(numero, 9, numero * 9))
+print("{} x {:>2} = {:>2}".format(numero, 10, numero * 10))
 print("===========")
 
 print("\nFim do programa.")   
