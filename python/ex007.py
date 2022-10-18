c63fc41148fd4a03cbc5aa09939b81c9def54ab3
Washington S Costa <wasanco@outlook.com>
2022-10-18 13:39:10 -0300

    Atualizacao ate ex007

---
+++ python/ex007.py
@@ -0,0 +1,8 @@
+# Faça um programa que lêia duas notas de um aluno, calcule e exiba a média.
+
+nota1 = float(input("Entre com a primeira nota: "))
+nota2 = float(input("Entre com a segunda nota: "))
+
+print("As notas digitadas foram: {} e {}".format(nota1, nota2))
+print("A média do aluno é {}".format((nota1 + nota2) / 2))
+print("fim do programa.")
