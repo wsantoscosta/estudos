11260844cee87f1375877b9638fa57de581a482a
washington <wasanco@outlook.com>
2023-11-09 14:08:42 -0300

    JS Aula 11 Ex008 Ex009 Ex010

---
+++ javascript/aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 81
+console.log(`A velocidade do seu veiculo é ${velocidade} km/h.`)
+if (velocidade > 80) {
+    console.log("Você foi multado por exceder o limite permitido!.")
+}
+
+console.log("Dirija sempre usando cinto de segurança!")
