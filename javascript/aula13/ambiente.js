32e0fa2efeb8bd8cbdf8f124662039795b0e94de
washington <wasanco@outlook.com>
2023-12-08 12:48:59 -0300

    JS Aula14 Laços While Do While

---
+++ javascript/aula13/ambiente.js
@@ -0,0 +1,15 @@
+var contador = 1
+// Exemplo while
+/*
+while (contador <= 6) {
+    console.log('Olá, td bem!?')
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/
+// Exemplo Do While
+
+do {
+    console.log(`Feito passo ...${contador}`)
+    contador++
+} while (contador <= 6)
