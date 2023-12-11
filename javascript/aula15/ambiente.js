e0e80c7a28b6a55409d4de149f2bfd42f3fdfa48
washington <wasanco@outlook.com>
2023-12-11 12:48:11 -0300

    JS Aula15 Array/Vetor

---
+++ javascript/aula15/ambiente.js
@@ -0,0 +1,23 @@
+let num = [5, 8, 4]
+
+num[3] = 6
+num.push(7)
+
+console.log(`O nosso vetor é o ${num}`)
+
+console.log('O vetor será ordenado...')
+num.sort()
+
+console.log(`O nosso vetor ordenado é o ${num}`)
+
+console.log(`O vetor tem ${num.length} posições`)
+
+console.log(`A primeira posição do vetor agora tem o valor ${num[0]}`)
+
+let pos = 8
+
+console.log(`O número 8 está na posição ${num.indexOf(pos)}`)
+
+
+
+
